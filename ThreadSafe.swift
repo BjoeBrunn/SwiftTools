a5fc2c6841e947415f5e438daa886dd9f71ba660
BjoeBrunn <BjoeBrunn@users.noreply.github.com>
2022-01-20 18:46:57 +0100

    Update ThreadSafe.swift

--- ThreadSafe.swift
+++ ThreadSafe.swift
@@ -1,24 +1,24 @@
 @propertyWrapper
-struct ThreadSafe {
-  private var value: T
-  private let queue: DispatchQueue
-  
-  init(wrappedValue value:T, queue:DispatchQueue) {
-    self.value = value
-    self.queue = queue
-  }
-  
-  var wrappedValue: T {
-    get { return queue.sync { value } }
-    set {
-      queue.sync {
-        value = newValue
-      }
+struct ThreadSafe<T> {
+    private var value: T
+    private let queue: DispatchQueue
+    
+    init(wrappedValue value:T, queue:DispatchQueue = DispatchQueue(label: "BBThreadSafe")) {
+        self.value = value
+        self.queue = queue
     }
-  }
-  
-  var projectedValue: T {
-    get { return value }
-    set { value = newValue }
-  }
+    
+    var wrappedValue: T {
+        get { return queue.sync { value } }
+        set {
+            queue.sync(flags: .barrier) {
+                value = newValue
+            }
+        }
+    }
+    
+    var projectedValue: T {
+        get { return value }
+        set { value = newValue }
+    }
 }
